39c0937f94ea991a63a0cdf45884e81ced0f2931
Gerard López Villena <gerardlopezvillena@gmail.com>
2018-08-24 02:12:43 +0200

    Primer Pas

--- webempresarial/urls.py
+++ webempresarial/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    path('about/',views.about,name='about'),
+    path('blog/',views.blog,name='blog'),
+    path('contact/',views.contact,name='contact'),
+    path('sample/',views.sample,name='sample'),
+    path('services/',views.services,name='services'),
+    path('store/',views.store,name='store'),
 ]
