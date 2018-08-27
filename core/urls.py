25189f9c993d8a6d293d3e9537b380041ee35a22
Gerard López Villena <gerardlopezvillena@gmail.com>
2018-08-27 03:16:44 +0200

    Mix Front/Back End 2

--- core/urls.py
+++ core/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    # Paths Admin
-    path('admin/', admin.site.urls),
     # Paths Core
-    path('', include('core.urls')),
+    path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('blog/',views.blog,name='blog'),
+    path('contact/',views.contact,name='contact'),
+    path('sample/',views.sample,name='sample'),
+    path('services/',views.services,name='services'),
+    path('store/',views.store,name='store'),
 ]
+
