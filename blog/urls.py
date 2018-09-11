3574f624295e2f183f8a0acb85912b4cb1fe2dfd
Gerard López Villena <gerardlopezvillena@gmail.com>
2018-09-11 02:51:19 +0200

    View/URL Category

--- blog/urls.py
+++ blog/urls.py
@@ -17,8 +17,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Paths Blog
     path('',views.blog,name='blog'),
+    path('category/<int:category_id>/',views.category,name='category'),
 ]
 
 
